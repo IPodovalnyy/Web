016af8d1e11e281d43ec03405268599d51ce33e0
IPodovalnyy <Podovalniy@yandex.ru>
2020-03-13 15:30:59 +0700

    Исправлены замечания по задаче температуры

--- Temperatures/Temperatures.js
+++ Temperatures/Temperatures.js
@@ -1,45 +1,57 @@
-function pressEnter(event) {
-    if (event.keyCode === 13) {
-        convertTemperature();
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var inputTemperature = document.getElementById("input_temperature");
+    inputTemperature.addEventListener("keypress", pressEnter);
+
+    var buttonConvert = document.querySelector(".get_temperature button");
+    buttonConvert.addEventListener("click", convertTemperature);
+});
 
 function convertTemperature() {
     var input = document.getElementById("input_temperature");
 
     if (input.value.trim() === "") {
         clearInputOutputField(input);
-        return 0;
+        return;
     }
 
     var temperatureC = Number(input.value.replace(",", "."));
 
     if (isNaN(temperatureC)) {
         clearInputOutputField(input);
-        return 0;
+        return;
     }
 
-    setTemperature(document.getElementById("temperatureF"), toFahrenheit(temperatureC));
-    setTemperature(document.getElementById("temperatureK"), toKelvin(temperatureC));
+    setTemperature(document.getElementById("temperatureF"), convertToFahrenheit(temperatureC));
+    setTemperature(document.getElementById("temperatureK"), convertToKelvin(temperatureC));
 }
 
 function setTemperature(outPutField, outTemperature) {
     outPutField.innerHTML = outTemperature.toFixed(3);
-    outPutField.style.visibility = "visible";
+    outPutField.classList.remove("set_hidden_text");
+    outPutField.classList.add("set_visible_text");
 }
 
-function toFahrenheit(temperatureC) {
+function convertToFahrenheit(temperatureC) {
     return temperatureC * 1.8 + 32;
 }
 
-function toKelvin(temperatureC) {
-    const coefficientKelvin = 273.15;
+function convertToKelvin(temperatureC) {
+    var coefficientKelvin = 273.15;
     return temperatureC + coefficientKelvin;
 }
 
 function clearInputOutputField(input) {
     alert("Введите число");
     input.value = "";
-    document.getElementById("temperatureF").style.visibility = "hidden";
-    document.getElementById("temperatureK").style.visibility = "hidden";
+
+    document.getElementById("temperatureF").classList.remove("set_visible_text");
+    document.getElementById("temperatureF").classList.add("set_hidden_text");
+    document.getElementById("temperatureK").classList.remove("set_visible_text");
+    document.getElementById("temperatureK").classList.add("set_hidden_text");
+}
+
+function pressEnter(event) {
+    if (event.key === 13) {
+        convertTemperature();
+    }
 }

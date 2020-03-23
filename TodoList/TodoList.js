890cb25e0a3b1afe0e4e3bcd855ccf2326cf7abb
IPodovalnyy <Podovalniy@yandex.ru>
2020-03-23 17:05:41 +0700

    Исправлены замечания по задаче

--- TodoList/TodoList.js
+++ TodoList/TodoList.js
@@ -3,115 +3,106 @@ document.addEventListener("DOMContentLoaded", function () {
     addButton.addEventListener("click", addRecordToList);
 
     var inputRecord = document.getElementById("input_record");
-    inputRecord.addEventListener("keypress", function () {
+    inputRecord.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
             addRecordToList();
         }
     });
-});
 
-function addRecordToList() {
-    var input = document.getElementById("input_record");
-    var record = input.value;
+    function addRecordToList() {
+        var input = document.getElementById("input_record");
+        var record = input.value;
 
-    if (record.trim() === "") {
-        alert("Введите запись");
-        return;
-    }
-    input.value = "";
-
-    var tableRecord = document.getElementById("tableRecords");
-    var bodyTable = tableRecord.getElementsByTagName("tbody")[0];
-    var numRows = bodyTable.rows.length;
-
-    bodyTable.insertRow(numRows);
-    var row = bodyTable.getElementsByTagName("tr")[numRows];
-
-    var td1 = document.createElement("td");
-    td1.innerHTML = record;
-
-    var buttonDel = document.createElement("button");
-    buttonDel.innerHTML = "Удалить<br>запись";
-    buttonDel.setAttribute("type", "button");
-    buttonDel.addEventListener("click", function () {
-        var row = this.parentElement.parentElement; // родитель кнопки td, родитель td строка
-        row.parentElement.removeChild(row);
-        displayScrollBar(bodyTable);
-    });
-
-    var buttonEdit = document.createElement("button");
-    buttonEdit.innerHTML = "Изменить<br>запись";
-    buttonEdit.addEventListener("click", function () {
-        var row = this.parentElement.parentElement;
-        createEditRow(row);
-    });
-
-    var td2 = document.createElement("td");
-    td2.appendChild(buttonDel);
-    td2.appendChild(buttonEdit);
-
-    row.appendChild(td1);
-    row.appendChild(td2);
-
-    displayScrollBar(bodyTable);
-}
-
-function createEditRow(rowToEdit) {
-    rowToEdit.style.display = "none";
-
-    var inputEditRecord = document.createElement("input");
-    inputEditRecord.setAttribute("type", "text");
-    inputEditRecord.value = rowToEdit.cells[0].innerText;
-
-    var td1 = document.createElement("td");
-    td1.appendChild(inputEditRecord);
-
-    var buttonCancel = document.createElement("button");
-    buttonCancel.innerHTML = "Отменить";
-    buttonCancel.addEventListener("click", function () {
-        row.parentElement.removeChild(row);
-        rowToEdit.style.display = "table";
-    });
-
-    var buttonSave = document.createElement("button");
-    buttonSave.innerHTML = "Применить";
-    buttonSave.addEventListener("click", function () {
-        var row = this.parentElement.parentElement;
-        var newRecord = row.getElementsByTagName("input")[0].value;
-        if (newRecord.trim() === "") {
+        if (record.trim() === "") {
             alert("Введите запись");
             return;
         }
-        rowToEdit.cells[0].innerHTML = newRecord;
-        row.parentElement.removeChild(row);
-        rowToEdit.style.display = "table";
-    });
+        input.value = "";
 
-    var td2 = document.createElement("td");
-    td2.appendChild(buttonCancel);
-    td2.appendChild(buttonSave);
+        var tableRecord = document.getElementById("tableRecords");
+        var listBodyTable = tableRecord.getElementsByTagName("tbody");
 
-    var bodyTable = document.getElementsByTagName("tbody")[0];
+        if (listBodyTable.length === 0) {
+            var bodyTable = document.createElement("tbody");
+            tableRecord.appendChild(bodyTable);
+        } else {
+            bodyTable = listBodyTable[0];
+        }
 
-    bodyTable.insertRow(rowToEdit.rowIndex);
-    var row = bodyTable.getElementsByTagName("tr")[rowToEdit.rowIndex];
+        var numRows = bodyTable.rows.length;
 
-    row.appendChild(td1);
-    row.appendChild(td2);
-}
+        bodyTable.insertRow(numRows);
+        var row = bodyTable.getElementsByTagName("tr")[numRows];
 
-function displayScrollBar(bodyTable) {
-    var paddingIncrease = 5;
-    var height = bodyTable.scrollHeight;
-    var maxHeight = Number(window.getComputedStyle(bodyTable).maxHeight.replace(/\D/g, ""));
+        var td1 = document.createElement("td");
+        td1.innerHTML = record;
 
-    if (height > maxHeight + paddingIncrease) {
-        bodyTable.classList.remove("overflowBodyTableHidden");
-        bodyTable.classList.add("overflowBodyTableAuto");
+        var buttonDel = document.createElement("button");
+        buttonDel.innerHTML = "Удалить<br>запись";
+        buttonDel.setAttribute("type", "button");
+        buttonDel.addEventListener("click", function () {
+            var row = this.parentElement.parentElement; // родитель кнопки td, родитель td строка
+            row.parentElement.removeChild(row);
+        });
+
+        var buttonEdit = document.createElement("button");
+        buttonEdit.innerHTML = "Изменить<br>запись";
+        buttonEdit.addEventListener("click", function () {
+            var row = this.parentElement.parentElement;
+            createEditRow(row);
+        });
+
+        var td2 = document.createElement("td");
+        td2.appendChild(buttonDel);
+        td2.appendChild(buttonEdit);
+
+        row.appendChild(td1);
+        row.appendChild(td2);
     }
-    else {
-        bodyTable.classList.remove("overflowBodyTableAuto");
-        bodyTable.classList.add("overflowBodyTableHidden");
 
+    function createEditRow(rowToEdit) {
+        rowToEdit.style.display = "none";
+
+        var inputEditRecord = document.createElement("input");
+        inputEditRecord.setAttribute("type", "text");
+        inputEditRecord.value = rowToEdit.cells[0].innerText;
+
+        var td1 = document.createElement("td");
+        td1.appendChild(inputEditRecord);
+
+        var buttonCancel = document.createElement("button");
+        buttonCancel.innerHTML = "Отменить";
+        buttonCancel.addEventListener("click", function () {
+            row.parentElement.removeChild(row);
+            rowToEdit.style.display = "table";
+        });
+
+        var buttonSave = document.createElement("button");
+        buttonSave.innerHTML = "Применить";
+        buttonSave.addEventListener("click", function () {
+            var row = this.parentElement.parentElement;
+            var newRecord = row.getElementsByTagName("input")[0].value;
+
+            if (newRecord.trim() === "") {
+                alert("Введите запись");
+                return;
+            }
+
+            rowToEdit.cells[0].innerHTML = newRecord;
+            row.parentElement.removeChild(row);
+            rowToEdit.style.display = "table";
+        });
+
+        var td2 = document.createElement("td");
+        td2.appendChild(buttonCancel);
+        td2.appendChild(buttonSave);
+
+        var bodyTable = document.getElementsByTagName("tbody")[0];
+
+        bodyTable.insertRow(rowToEdit.rowIndex);
+        var row = bodyTable.getElementsByTagName("tr")[rowToEdit.rowIndex];
+
+        row.appendChild(td1);
+        row.appendChild(td2);
     }
-}
+});

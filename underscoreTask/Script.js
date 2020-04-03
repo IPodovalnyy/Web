dd6c7b4a4b787b3083e4cb5a7408609ebb18988e
IPodovalnyy <Podovalniy@yandex.ru>
2020-04-03 17:49:34 +0700

    Внесены измененеия в задачу по underscore

--- underscoreTask/Script.js
+++ underscoreTask/Script.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
+    function getAverageAge(people) {
+        return _.reduce(people, function (sum, p) {
+            return sum + p.age;
+        }, 0) / people.length;
+    }
+
+    function getRangeAge(people) {
+        return _.chain(people)
+            .filter(function (p) {
+                return p.age >= 20 && p.age <= 30;
+            })
+            .sortBy(function (man) {
+                return man.age;
+            })
+            .value();
+    }
+
+    function addFullName(people) {
+        return _.map(people, function (man) {
+            return _.extend(man, {fullName: [man.lastName] + " " + [man.name]})
+        });
+
+    }
+
     var people = [
         {
             age: 25,
@@ -52,34 +76,16 @@ $(document).ready(function () {
         }
     ];
 
-    var averageAge = _.reduce(people, function (sum, p) {
-            return sum + p.age;
-        }, 0) / people.length;
+    console.log("Средний возраст: " + getAverageAge(people));
 
-    console.log ("Средний возраст: " + averageAge);
-
-    var filteredPeople = _.chain(people)
-        .filter(function (p) {
-            return p.age >= 20 && p.age <= 30;
-        })
-        .sortBy(function (man) {
-            return man.age;
-        })
-        .value();
-
-    console.log ("Люди от 20 до 30 лет: ");
-    _.each(filteredPeople, function (p) {
+    console.log("\nЛюди от 20 до 30 лет: ");
+    _.each(getRangeAge(people), function (p) {
         console.log(p.name + ": " + p.age);
     });
 
-
-    var peopleFullName = _.map(people, function (man) {
-        return _.extend(man, {fullName: [man.lastName] + " " + [man.name]})
+    console.log("\nПолные имена");
+    var fullName = addFullName(people);
+    _.each(fullName, function (p) {
+        console.log(p.fullName + ": " + p.age);
     });
-
-    console.log ("Полные имена");
-    for (var fullName in peopleFullName) {
-        //noinspection JSUnfilteredForInLoop
-        console.log(fullName + ": " + peopleFullName[peopleFullName]);
-    }
 });
